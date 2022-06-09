54d31a3c96c1f8a73f29c0a81084f9c0ac1f94c6
toto6038 <50100922+toto6038@users.noreply.github.com>
2022-06-10 02:21:30 +0800

    feat: add routes and page template

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.debug=True
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# hello後的網址會被接收為變數，可用來傳進template當作內容的一部分
+@app.route("/hello/<name>")
+def hello_name(name):
+    return render_template('hello.html', name=name)
