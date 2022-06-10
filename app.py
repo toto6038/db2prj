e2cd0267b5d1ad35f7188695eea1366334f257f0
haoching918 <haoching918@gmail.com>
2022-06-10 16:35:23 +0800

    dropdown button

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.debug=True
 def index():
     return render_template('index.html')
 
-# hello後的網址會被接收為變數，可用來傳進template當作內容的一部分
+
 @app.route("/hello/<name>")
 def hello_name(name):
     return render_template('hello.html', name=name)
