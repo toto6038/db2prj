7f9d9e6cde2ce51feb6399b23030c7693e1e32ba
haoching918 <haoching918@gmail.com>
2022-06-12 14:55:53 +0800

    create:an automap classes to corresponding existed table

--- app.py
+++ app.py
@@ -1,11 +1,50 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+
 #  引入form類別
 from view_form import UserForm
 
+from sqlalchemy.ext.automap import automap_base
+
 app = Flask(__name__)
-app.config['SECRET_KEY']='your key'
 app.debug=True
+
+app.config['SECRET_KEY']='your key'
+
+# 連接到mysql
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://team7:nomoredatabse@mc.toto6038.dev:8895/final_prj'
+db = SQLAlchemy(app)
+# 設定sqlalchemy自動更跟蹤資料庫
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+# 查詢時會顯示原始SQL語句
+app.config['SQLALCHEMY_ECHO'] = True
+# 禁止自動提交資料處理
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = False
+
+
+Base = automap_base()
+Base.prepare(db.engine, reflect = True)
+
+# 建立 table class
+User = Base.classes.user
+Manufacturer = Base.classes.manufacturer
+#Favor = Base.classes.favor
+Laptop = Base.classes.laptop
+Product = Base.classes.product
+Purchase = Base.classes.purchase
+Ram= Base.classes.ram
+Shop = Base.classes.shop
+Storage = Base.classes.storage
+
+@app.route('/test')
+def test():
+    # new_user = User(ID='123456',name='Tommy',password='12345', address="Taipei",regDate='1975-03-01 12:04:12')
+    # db.session.add(new_user)
+    # db.session.commit()
+    # r = db.session.query(User).all()
+    # for i in r:
+    #     print(i.name)
+    return render_template('index.html')
 
 @app.route('/')
 def index():
@@ -31,6 +70,7 @@ def login():
     if form.validate_on_submit():
         return 'Success Submit'
     #  如果不是提交過來的表單，就是GET，這時候就回傳user.html網頁
+
     return render_template('login.html', form=form)
 
 @app.errorhandler(404)
