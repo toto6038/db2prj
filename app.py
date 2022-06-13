ee7205a06b1fb64cf58dbc2c01a1e97e5ccbb925
haoching918 <haoching918@gmail.com>
2022-06-13 16:02:47 +0800

    feat:register page

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 
 #  引入form類別
-from view_form import UserForm
+from view_form import UserForm, RegForm
 
 from sqlalchemy.ext.automap import automap_base
 
 app = Flask(__name__)
 app.debug=True
 
-app.config['SECRET_KEY']='your key'
+app.config['SECRET_KEY']=b'\xef\x01w8\xcd\xe5\xf3!\xc1\xc2\x81k\x12\n\xd7P'
 
 # 連接到mysql
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://team7:nomoredatabse@mc.toto6038.dev:8895/final_prj'
 db = SQLAlchemy(app)
-# 設定sqlalchemy自動更跟蹤資料庫
+# 設定sqlalchemy自動跟蹤資料庫
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 # 查詢時會顯示原始SQL語句
 app.config['SQLALCHEMY_ECHO'] = True
@@ -28,7 +28,7 @@ Base.prepare(db.engine, reflect = True)
 # 建立 table class
 User = Base.classes.user
 Manufacturer = Base.classes.manufacturer
-#Favor = Base.classes.favor
+#Favors = Base.classes.favors
 Laptop = Base.classes.laptop
 Product = Base.classes.product
 Purchase = Base.classes.purchase
@@ -52,7 +52,7 @@ def index():
 
 # hello後的網址會被接收為變數，可用來傳進template當作內容的一部分
 @app.route("/hello/<name>")
-def hello_name(name):
+def hello_name(name:str):
     return render_template('hello.html', name=name)
 
 @app.route("/about")
@@ -67,11 +67,30 @@ def member():
 def login():
     form = UserForm()
     #  flask_wtf類中提供判斷是否表單提交過來的method，不需要自行利用request.method來做判斷
+    name=None
     if form.validate_on_submit():
-        return 'Success Submit'
+        name=form.username.data
+        form.username.data=''
+        return redirect(url_for('hello_name', name=name))
     #  如果不是提交過來的表單，就是GET，這時候就回傳user.html網頁
 
     return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegForm()
+    if form.validate_on_submit():
+        if valid_usrname(form.username.data):
+            # 頁面並無 ID 及 address 
+            db.session.add(User(ID ='848101', name=form.username.data, password=form.password.data, address='2qqweqw', admin=form.admin.data))
+            db.session.commit()
+            return redirect(url_for('hello_name', name=form.username.data))
+    return render_template('register.html', form=form)
+def valid_usrname(name):
+    for nm in db.session.query(User).all():
+        if nm.name == name:
+            return False
+    return True
 
 @app.errorhandler(404)
 @app.errorhandler(500)
@@ -83,4 +102,4 @@ def pageNotFound(error):
         errMsg='Internal server error'
     else:
         errMsg=''
-    return render_template('404.html', errCode=errCode, errMsg=errMsg), 404
+    return render_template('404.html', errCode=errCode, errMsg=errMsg), error.code
