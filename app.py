a1b2db211aaca7f6fce45e6cc17106b1dd57f01f
haoching918 <haoching918@gmail.com>
2022-06-15 11:31:17 +0800

    add:ram page, icon need to fix

--- app.py
+++ app.py
@@ -191,11 +191,6 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-
-def get_img(data):
-    r = []
-    for d in data:
-        r[d.model] = ''
 # find product laptop
 @app.route('/laptop/positioning/<field>')
 def find_laptop_pos(field):
@@ -221,14 +216,32 @@ def find_laptop_weight(field):
     , table_Laptop.weight, table_Laptop.price, table_Laptop.disk_capacity).join(table_Product,table_Laptop.model 
     == table_Product.model).filter(table_Laptop.weight <= field )
     
-    flash(f"{data.count()} data were found for Weight : {field} kg")
+    flash(f"{data.count()} data were found for Weight : {field} Kg")
     
     return render_template('laptop.html', data = data)
 
 # find product ram
-@app.route('/ram/<field>')
-def find_ram(field):
-    return render_template('ram.html')
+@app.route('/ram/capacity/<field>')
+def find_ram_cap(field):
+    data = db.session.query(table_Product.name, table_Ram.capacity, table_Ram.price, table_Ram.ddr_type).join(table_Product,table_Ram.model 
+    == table_Product.model).filter(table_Ram.capacity == field)
+
+    flash(f"{data.count()} data were found for Capacity : {field} GB")
+    return render_template('ram.html', data = data)
+@app.route('/ram/price/<field>')
+def find_ram_price(field):
+    data = db.session.query(table_Product.name, table_Ram.capacity, table_Ram.price, table_Ram.ddr_type).join(table_Product,table_Ram.model 
+    == table_Product.model).filter(table_Ram.price + 1000 > field, table_Ram.price < field)
+
+    flash(f"{data.count()} data were found for Price : $ {field} ")
+    return render_template('ram.html', data = data)
+@app.route('/ram/type/<field>')
+def find_ram_type(field):
+    data = db.session.query(table_Product.name, table_Ram.capacity, table_Ram.price, table_Ram.ddr_type).join(table_Product,table_Ram.model 
+    == table_Product.model).filter(table_Ram.ddr_type == field)
+
+    flash(f"{data.count()} data were found for Type : {field} ")
+    return render_template('ram.html', data = data)
 
 # find product storage
 @app.route('/storage/<field>')
